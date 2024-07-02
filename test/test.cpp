65d0e419326dba68b5feb74ea4957066b4f5dc12
Pascal Post <pascal-post@mailbox.org>
2024-07-02 19:10:52 +0200

    small test enhancement

--- test/test.cpp
+++ test/test.cpp
@@ -1,24 +1,22 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <future>
 
 #include "../include/helper.h"
 #include "../include/server.h"
 
 std::string send(const std::string &address, const unsigned port, const std::string &message) {
-    http_server::Socket socket{};
+    const http_server::Socket socket{};
     socket.connect(address, port);
     socket.write(message);
     return socket.read();
 }
 
-TEST_CASE("The http server is able to answer a http request from a client") {
-    constexpr int port{4221};
+constexpr int port{4221};
 
-    auto server = std::async([]() {
+TEST_CASE("The http server is able to answer a http request from a client") {
+    auto server_process = std::async([]() {
         const http_server::Server server(port, 5);
         server.accept();
     });
@@ -30,14 +28,11 @@ TEST_CASE("The http server is able to answer a http request from a client") {
 
     response.wait();
     const std::string response_message = response.get();
-
     REQUIRE(response_message.starts_with("HTTP/1.1 200 OK"));
 }
 
 TEST_CASE("The http server sends a 404 on an unknown route") {
-    constexpr int port{4221};
-
-    auto server = std::async([]() {
+    auto server_process = std::async([]() {
         const http_server::Server server(port, 5);
         server.accept();
     });
@@ -49,6 +44,5 @@ TEST_CASE("The http server sends a 404 on an unknown route") {
 
     response.wait();
     const std::string response_message = response.get();
-
     REQUIRE(response_message.starts_with("HTTP/1.1 404 Not Found"));
 }
